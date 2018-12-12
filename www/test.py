64755e8e037e2cc921d8ebdb226edf8d8a7dcba3
leiphp <tian-lei-happy@163.com>
2018-12-12 14:48:05 +0800

    添加测试代码，可实现数据表user数据添加

---
+++ www/test.py
@@ -0,0 +1,17 @@
+import asyncio
+import orm
+import random
+from models import User,Blog,Comment
+
+async def test(loop):
+    await orm.create_pool(loop,user='www-data',password='www-data',db='awesome')
+    u =User(name='test',email='test%s@example.com' % random.randint(0,10000000),passwd='abc123456',image='about:blank')
+    await u.save()
+
+
+#要运行协程，需要使用事件循环 
+if __name__ == '__main__':
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(test(loop))
+        print('Test finished.')
+        loop.close()
