bfa7654e8fbaa0c2011e9ac1d2b19298222a182d
leiphp <tian-lei-happy@163.com>
2018-12-12 11:40:29 +0800

    修改访问出现下载的问题

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from aiohttp import web
 
 def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>')
+	return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
 
 @asyncio.coroutine
 def init(loop):
