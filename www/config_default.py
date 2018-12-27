a6acc2e4a21a4c37843b271eceb36c3c50cf7e88
leiphp <tian-lei-happy@163.com>
2018-12-27 15:01:06 +0800

    update config

--- www/config_default.py
+++ www/config_default.py
@@ -13,7 +13,7 @@ configs = {
         'host': '127.0.0.1',
         'port': 3306,
         'user': 'root',
-        'password': 'tianyi#leixt',
+        'password': 'root',
         'db': 'awesome'
     },
     'session': {
